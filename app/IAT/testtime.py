0566c7be242fdc77757fd9f914163c01b6edbbb2
Jessicajq <32897703+Jessicajq@users.noreply.github.com>
2021-06-23 16:05:36 +0800

    Delete app directory

--- app/IAT/testtime.py
+++
@@ -1,9 +0,0 @@
-# -*- coding:utf-8 -*-
-'''
-@Time :2021/5/31$
-@Author : xiaojing
-@File :testtime.py
-'''
-from datetime import datetime
-now = datetime.now().strftime('%Y-%m-%d_%H_%M_%S')
-print(now)
